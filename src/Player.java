dcdb6421632e35229f486589f26bfdc67286b7c9
JakoAlltrades <Jpriemo@gmail.com>
2015-10-30 14:58:43 -0600

    The player class

--- src/Player.java
+++ src/Player.java
@@ -6,17 +6,30 @@ public class Player
 	private Color col;
 	private int score1 = 0;
 	private int score2 = 0;
+	private int ID = 0;
 	public Player(String n,Color c){
 		name = n;
 		col = c;
+		
 	}
 	
-	public void addPoint1s(){
+	public void addScore1(){//adds a point to the score1 variable
 		score1++;
 	}
+	public void minusScore1(){//subtracts a point to the score1 variable
+		score1--;
+	}
 	
-	public void addScore2(){
+	public void addScore2(){//adds a point to score2 variable
 		score2++;
+	}
+	
+	public void setID(int data){//sets the ID (used when to find out the order player gets there turn
+		ID = data;
+	}
+	
+	public int getID(){//returns the ID variable(can be used to check the order or let the player get his/her turn
+		return ID;
 	}
  
 	
