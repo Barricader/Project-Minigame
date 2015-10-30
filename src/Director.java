79fc8ecda98affc9e267a305602d5ac5d0333f64
Joseph Jones <jjones15470@hotmail.com>
2015-10-30 15:28:47 -0600

    Init director

--- src/Director.java
+++ src/Director.java
@@ -8,11 +8,13 @@ public class Director {
 	// States of the board
 	private final byte START = 0, BOARD = 1, MINIGAME = 2, END = 3;
 	private final byte MAX_WEIGHT = 100;
+	private final byte MAX_GAMES = 20;
 	private byte curPlayer;
 	private byte state;
 	private int turn;
 	private int maxTurns;
-	private MiniGame[][] minigames;
+	private Minigame[] minigames;
+	private byte[] minigameWeight;
 	
 	public Director (int maxTurns) {
 		this.maxTurns = maxTurns;
@@ -20,5 +22,9 @@ public class Director {
 		turn = 1;
 		curPlayer = 1;
 		
+		for (byte i = 0; i < MAX_GAMES; i++) {
+			minigames[i]= new Minigame(i);
+			minigameWeight[i] = 0;
+		}
 	}
 }
---
+++ src/Minigame.java
@@ -0,0 +1,15 @@
+/**
+ * A minigame
+ * @author JoJones
+ *
+ */
+public class Minigame {
+	private final byte ID;
+	private String name;
+	private String inst;
+	private int[] rankValues;
+	
+	public Minigame(byte id) {
+		this.ID = id;
+	}
+}
