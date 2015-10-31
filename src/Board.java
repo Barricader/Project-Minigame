f0854546ccc02c5b483fc99b1241cbdbc210eb17
Joseph Jones <jjones15470@hotmail.com>
2015-10-31 17:14:16 -0600

    Setting up more

--- src/Board.java
+++ src/Board.java
@@ -4,5 +4,15 @@
  *
  */
 public class Board {
-	
+	private final byte MAX_TILES = 80;
+	private Tile[] tiles;
+	/**
+	 * Create a board
+	 */
+	public Board() {
+		// Init tiles
+		for (int i = 0; i < MAX_TILES; i++) {
+			tiles[i] = new Tile();
+		}
+	}
 }
--- src/Director.java
+++ src/Director.java
@@ -1,4 +1,5 @@
-import mini.*;
+import mini.Minigame;
+import mini.Test;
 
 /**
  * 
@@ -8,25 +9,71 @@ import mini.*;
  */
 public class Director {
 	// States of the board
+	// I made them bytes to optimize memory usage
 	private final byte START = 0, BOARD = 1, MINIGAME = 2, END = 3;
 	private final byte MAX_WEIGHT = 100;
 	private final byte MAX_GAMES = 20;
 	private byte curPlayer;
+	private Player[] players;
 	private byte state;
 	private int turn;
 	private int maxTurns;
 	private Minigame[] minigames;
 	private byte[] minigameWeight;
+	private Board board;
 	
-	public Director (int maxTurns) {
+	/**
+	 * Create a Director object
+	 * @param maxTurns - Amount of max turns in the current game
+	 * @param players - Players to play in the game
+	 */
+	public Director (int maxTurns, byte players) {
 		this.maxTurns = maxTurns;
-		state = START;
-		turn = 1;
-		curPlayer = 1;
+		this.state = START;
+		this.turn = 1;
+		this.curPlayer = 1;
 		
+		board = new Board();
+		
+		// Init players here
+		for (byte i = 0; i < players; i++) {
+			this.players[i] = new Player();
+		}
+		
+		// Init minigames here
 		for (byte i = 0; i < MAX_GAMES; i++) {
-			minigames[i]= new Test(i);
-			minigameWeight[i] = 0;
+			this.minigames[i]= new Test(i);
+			this.minigameWeight[i] = 0;
+		}
+		
+		loop();
+	}
+	
+	/**
+	 * Main game loop
+	 */
+	public void loop() {
+		while (state != END) {
+			// Main game loop here
+			
+			if (state == BOARD) {
+				
+			}
+			else if (state == MINIGAME) {
+				
+			}
+			
+			if (curPlayer == players.length - 1) {
+				curPlayer = 0;
+			}
+			curPlayer++;
+			
+			if (state == BOARD) {
+				state = MINIGAME;
+			}
+			else {
+				state = BOARD;
+			}
 		}
 	}
 }
