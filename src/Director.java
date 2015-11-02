e9996718d358d3e3e56418b29a8a09262c5b2d80
Joseph Jones <jjones15470@hotmail.com>
2015-11-02 09:37:34 -0700

    uh

--- src/Director.java
+++ src/Director.java
@@ -1,41 +1,128 @@
-import mini.*;
+import java.util.Random;
+
+import mini.Minigame;
+import mini.Test;
 
 /**
  * 
  * Main class that will keep track of the game
- * You should create this after the player registration is done
  * @author JoJones
  *
  */
 public class Director {
 	// States of the board
+	// I made them bytes to optimize memory usage
 	private final byte START = 0, BOARD = 1, MINIGAME = 2, END = 3;
 	private final byte MAX_WEIGHT = 100;
 	private final byte MAX_GAMES = 20;
 	private byte curPlayer;
+	private Player[] players;
 	private byte state;
 	private int turn;
 	private int maxTurns;
 	private Minigame[] minigames;
 	private byte[] minigameWeight;
+	private Board board;
+	private Random r;
 	
 	/**
-	 * 
-	 * @param maxTurns - Set the max turns for the game before game over
+	 * Create a Director object
+	 * @param maxTurns - Amount of max turns in the current game
+	 * @param players - Players to play in the game
 	 */
-	public Director (int maxTurns) {
+	public Director (int maxTurns, byte players) {
 		this.maxTurns = maxTurns;
-		state = START;
-		turn = 1;
-		curPlayer = 1;
+		this.state = BOARD;
+		this.turn = 1;
+		this.curPlayer = 0;
 		
-		for (byte i = 0; i < MAX_GAMES; i++) {
-			minigames[i]= new Test(i);
-			minigameWeight[i] = 0;
+		board = new Board();
+		r = new Random();
+		
+		// Init players here
+		this.players = new Player[players];
+		for (byte i = 0; i < players; i++) {
+			this.players[i] = new Player();
 		}
+		
+		// Init minigames here
+		this.minigames = new Minigame[MAX_GAMES];
+		this.minigameWeight = new byte[MAX_WEIGHT];
+		for (byte i = 0; i < MAX_GAMES; i++) {
+			this.minigames[i]= new Test(i);
+			this.minigameWeight[i] = 0;
+		}
+		
+		loop();
 	}
 	
+	/**
+	 * Main game loop
+	 */
 	public void loop() {
-		
+		if (state == START) {
+			// Start stuff
+		}
+		while (state != END) {
+			// Main game loop here
+			
+			// Check what state we are in
+			if (state == BOARD) {
+				boolean isTurn = true;
+				while (isTurn) {
+					// Player does stuff on board like move
+					isTurn = false;
+				}
+				
+				// Other stuff
+			}
+			else if (state == MINIGAME) {
+				// Check if minigame has been played recently
+				boolean picked = false;
+				byte curMinigame = -1;
+				while (!picked) {
+					curMinigame = (byte)r.nextInt(MAX_GAMES);
+					// Weight check
+					if (minigameWeight[curMinigame] <= 50) {
+						double defaultChance = 100 / MAX_GAMES;
+						// Change this is max games changes
+						// REDO this so that everything has an equal chance disregarding weight
+						defaultChance -= 0.1 * minigameWeight[curMinigame];
+						if ((double)r.nextInt(100) < defaultChance) {
+							picked = true;
+						}
+					}
+				}
+				
+				System.out.println(curMinigame);
+				
+				// Play minigame here
+				
+				// Update weights after minigame has been played
+				for (int i = 0; i < MAX_GAMES; i++) {
+					minigameWeight[i] -= 10;
+				}
+				minigameWeight[curMinigame] = MAX_WEIGHT;
+			}
+			
+			// Update game
+			if (curPlayer == players.length - 1) {
+				curPlayer = 0;
+			}
+			curPlayer++;
+			
+			if (state == BOARD) {
+				state = MINIGAME;
+			}
+			else {
+				state = BOARD;
+				turn++;
+			}
+			
+			// Check turn, if it is over max turns, the game is over
+			if (turn > maxTurns) {
+				state = END;
+			}
+		}
 	}
 }
