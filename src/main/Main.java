2da5471423b1dbfe71ee3db8371880da405ad869
David Kramer <dkramer@student.neumont.edu>
2015-11-02 14:24:40 -0700

    Changed 'Screen' to Main
    Main contains the JFrame for all other screen views
    that will be managed by the director.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,34 @@
 package main;
-public class Main {
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import screen.StartPanel;
+
+/**
+ * Main window of the application that provides a container for the active view.
+ * @author David Kramer
+ *
+ */
+public class Main extends JFrame {
+	public static final String TITLE = "Project MiniGame by Jo, Jack, and David";
+	public static final Dimension SIZE = new Dimension(1280, 720);
+	
+	private StartPanel startPanel;	// initial start panel
+
+	public Main() {
+		startPanel = new StartPanel();
+		add(startPanel);
+		
+		setTitle(TITLE);
+		setSize(SIZE);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		Director d = new Director(100, (byte)4);
+		Main testScreen = new Main();
 	}
 }
