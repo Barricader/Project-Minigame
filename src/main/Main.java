8e569b8f588fd50e35c992925f13a257551e3bc4
Joseph Jones <jjones15470@hotmail.com>
2015-11-05 14:16:24 -0700

    Adding a new director

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,22 +11,18 @@ import screen.StartPanel;
  * @author David Kramer
  *
  */
-public class Main extends JFrame implements Runnable {
+public class Main extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private static final String TITLE = "Project MiniGame by Jo, Jack, and David";
 	private static final Dimension SIZE = new Dimension(1280, 720);
 	
 	private StartPanel startPanel;	// initial start panel
-	private Director dir;
-	
-	// thread stuff
-	private Thread thread;
-	private boolean running = false;
+	private NewDirector dir;
 
 	public Main() {
 		startPanel = new StartPanel(this);
-		dir = new Director(this);
-		thread = new Thread(this);
+		dir = new NewDirector(this);
+		dir.start();
 		add(startPanel);
 		
 		setTitle(TITLE);
@@ -34,28 +30,9 @@ public class Main extends JFrame implements Runnable {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		thread.start();
 	}
 	
-	public void run() {
-		// Creating a limit on updates to 60 times a second
-		running = true;
-		long lastTime = System.nanoTime();
-		final double ns = 1000000000.0 / 60.0;
-		double delta = 0;
-		requestFocus();
-		while (running) {
-			long now = System.nanoTime();
-			delta += (now - lastTime) / ns;
-			lastTime = now;
-			while (delta >= 1) {
-				dir.loop();
-				delta--;
-			}
-		}
-	}
-	
-	public Director getDirector() {
+	public NewDirector getDirector() {
 		return dir;
 	}
 	
--- src/main/NewDirector.java
+++ src/main/NewDirector.java
@@ -1,10 +1,5 @@
 package main;
 import java.util.ArrayList;
-import java.util.Random;
-
-import mini.Minigame;
-import mini.Test;
-import screen.Board;
 
 // TODO: Make it output the player list sorting to test
 
@@ -16,12 +11,14 @@ import screen.Board;
  */
 public class NewDirector implements Runnable {
 	// States of the board
-	public static final byte START = 0, BOARD = 1, MINIGAME = 2, END = 3, INIT = 4;
+	public static final byte START = 0, BOARD = 1, MINIGAME = 2, END = 3, INIT = 4;//delete?
 	private ArrayList<Player> players;
-	private byte state;
+	private byte state;// CHANGE TO State
 	private int turn;
 	private int maxTurns;
 	private Main m;
+	private Thread t;
+	private boolean running;
 	
 	/**
 	 * Create a Director object
@@ -33,17 +30,71 @@ public class NewDirector implements Runnable {
 		this.turn = 1;
 		this.m = m;
 		
+		
+		
 		this.players = new ArrayList<Player>();
 	}
 	
-	public void update() {
-		
+	/**
+	 ** Start the thread
+	 **/
+	public synchronized void start() {
+		running = true;
+		t = new Thread(this, "Display");
+		t.start();
 	}
 	
-	public void render() {
-		
+	/**
+	 ** Stop the thread
+	 **/
+	public synchronized void stop() {
+		running = false;
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	private void update() {
+		// Do state update here
+	}
+	
+	private void render() {
+		// Do state render here
+	}
+
+	/**
+	 * Runs the update method 60 times a second.
+	 * Runs the render method as much as possible.
+	 */
+	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				update();
+				delta--;
+			}
+			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				m.setTitle("Thing | " + frames + " fps");
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	// TODO change to State
 	public void setState(byte state) {
 		this.state = state;
 	}
@@ -58,9 +109,5 @@ public class NewDirector implements Runnable {
 	
 	public void setTurns(int turns) {
 		maxTurns = turns;
-	}
-
-	public void run() {
-		
 	}
 }
