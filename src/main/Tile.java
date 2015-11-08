652c13e7565d3a0cdaeec85ab888c2930a31b64c
David Kramer <dkramer@student.neumont.edu>
2015-11-08 15:24:47 -0700

    Basic Perimeter Tiles Implemented
    * Tile sizes are dynamic, in that they resize if the overall window has
    been resized.
    
    * Basic player selection and movement. 
     - Not tied to tile though. You can click on a player and click to a new
    location and it will move there. (This will need to be changed to
    reflect movement from dice rolling onto different tiles!)
     
    * Changed style of dice to dark cyan theme.

--- src/main/Tile.java
+++ src/main/Tile.java
@@ -1,13 +1,26 @@
 package main;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import screen.GameUtils;
 
 /**
+ * A tile is a rectangular shape that is contained within the grid of the board. A tile
+ * has an (x,y) coordinate, a random color, and a random action or mini-game event. A tile
+ * also has an ID, which will be used to keep track of moving players, when the dice is
+ * rolled in the game. Based on the players' current position, whatever amount they roll,
+ * the tile that the player will land on will be their current tile + dice roll amount.
+ * The implementation for player movement will be handled in the player class, but that
+ * is the importance of ID's.
  * @author JoJones
- *
+ * @author David Kramer
  */
-public class Tile {
+public class Tile extends Rectangle {
 	public static final int ACTION_ADDSCORE1 = 0;
 	public static final int ACTION_SUBTRACT = 1;
 	public static final int ACTION_ADDSCORE2 = 2;
@@ -16,57 +29,32 @@ public class Tile {
 	private Color color;
 	private int action;
 	private int ID;
-	private int xPos;
-	private int yPos;
-	private int width;
-	private int height;
 	
-	public Tile(Color color, int action, int ID, int xPos, int yPos, int width, int height) {
+	/**
+	 * @deprecated	- We have sub-classed Rectangle
+	 */
+	private int xPos;
+	/**
+	 * @deprecated - We have sub-classed Rectangle
+	 */
+	private int yPos;
+	
+	/**
+	 * Constructs a new tile
+	 * @param color - Color of tile
+	 * @param action - Action of tile
+	 * @param ID - Tile ID for location
+	 * @param x - x coordinate
+	 * @param y - y coordinate
+	 * @param width - width of tile
+	 * @param height - height of tile
+	 */
+	public Tile(Color color, int action, int ID, int x, int y, int width, int height) {
+		super(x, y, width, height);
 		this.color = color;
 		this.action = action;
 		this.ID = ID;
-		this.xPos = xPos;
-		this.yPos = yPos;
-		this.width = width;
-		this.height = height;
 	}
-	
-	
-	public void setTileID(int id) {
-		ID = id;
-	}
-	
-	public int getTileID() {
-		return ID;
-	}
-	public void setAction(int act) {
-		action = act;
-	}
-	
-	public int getAction() {
-		return action;
-	}
-	
-	public int getXPos() {
-		return xPos;
-	}
-	
-	public int getYPos() {
-		return yPos;
-	}
-	
-	public int getWidth() {
-		return width;
-	}
-	
-	public int getHeight() {
-		return height;
-	}
-	
-	public Color getColor() {
-		return color;
-	}
-	
 	
 	/**
 	 * Performs an action on Player p based on the tiles action
@@ -81,22 +69,75 @@ public class Tile {
 		}
 		else if(action == ACTION_ADDSCORE2) {
 			if(p.getScore1() >= 10){
-				p.buyScore2();
+//				p.buyScore2();
 			}
 			
 		}
 	}
 	
-	
 	/**
-	 * Draw a tile
-	 * @param g - a Graphics object
+	 * Draws a tile to the board.
+	 * @param g - graphics context to draw to
 	 */
 	public void draw(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.drawRect(xPos, yPos, width, height);
-		g.setColor(color);
-		g.fillRect(xPos, yPos, width, height);
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setColor(Color.BLACK);
+		g2d.setStroke(new BasicStroke(2.0f));
+		g2d.drawRect(x * width, y * height, width, height);
+		g2d.setColor(color);
+		g2d.fillRect(x * width, y * height, width, height);
+		g2d.setColor(GameUtils.getInvertedColor(color));
+		g2d.setFont(new Font("Courier New", Font.BOLD, 20));
+		g2d.drawString("" + ID, (x * width) + 50, (y * height) + 50);
+	}
+	
+	// Mutator methods
+	
+	public void setAction(int act) {
+		action = act;
+	}
+	
+	public void setTileID(int id) {
+		ID = id;
+	}
+	
+	// Accessor methods
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getTileID() {
+		return ID;
+	}
+	
+	public int getAction() {
+		return action;
+	}
+	
+	public double getWidth() {
+		return width;
+	}
+	
+	public double getHeight() {
+		return height;
+	}
+	
+	/**
+	 * 
+	 * @deprecated - We have sub-classed Rectangle
+	 * @return
+	 */
+	public int getXPos() {
+		return xPos;
+	}
+	
+	/**
+	 * @deprecated - We have sub-classed Rectangle
+	 * @return
+	 */
+	public int getYPos() {
+		return yPos;
 	}
 	
 }
--- src/states/BoardState.java
+++ src/states/BoardState.java
@@ -2,54 +2,221 @@ package states;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
+
+import javax.swing.JPanel;
 
 import main.Dice;
 import main.NewDirector;
 import main.Player;
 import main.Tile;
 import screen.GameUtils;
+import screen.StatusPanel;
 
-public class BoardState extends State {
-	public static final byte TILE_COUNT = 10;
+/**
+ * The main board state of the game. A board is a collection of tiles that the player
+ * can land on to initiate random mini games. During game play, the active player
+ * can roll the dice provided from the board, to determine their next space that
+ * they will land on. Whenever this move is made, the event associated with the
+ * tile will then have to be completed by one or more players.
+ * @author David Kramer
+ *
+ */
+public class BoardState extends State implements ComponentListener, MouseListener, MouseMotionListener {
+	public static final byte HORIZONTAL_TILE_COUNT = 10;
+	public static final byte VERTICAL_TILE_COUNT = HORIZONTAL_TILE_COUNT / 2;
 	private static final long serialVersionUID = 1L;	
-	private NewDirector director;
-	private Rectangle midRect;
+	
+	private StatusPanel statusPanel;	// display active player and turns remaining
+	private Rectangle midRect;	// rect in the middle that contains game objects such as dice
 	private Dice dice;
 	private ArrayList<Tile> tiles;	// tiles of the board
 
+	/**
+	 * Constructs a new board state.
+	 * @param director - main director control of application
+	 */
 	public BoardState(NewDirector director) {
 		super(director);
-		this.director = director;
-		tiles = new ArrayList<>();
-		midRect = createMidRect();
-		dice = new Dice(500, 500);
-		createTiles();
+		init();
+	}
+
+	public void update() {
+		//TODO update director after player has moved
 	}
 	
-	private void createTiles() {
-		
-		// tile sizing
-		int tileWidth = 1280 / TILE_COUNT;
-		int tileHeight = 150;
-		
-		// horizontal tiles
-		for (int x = 0; x < TILE_COUNT; x++) {
-			Color color = GameUtils.getRandomColor();
-			int ID = x;
-			Tile t = new Tile(color, 0, ID, (x * tileWidth), 0, tileWidth, tileHeight);
-			tiles.add(t);
-		}
-		
-		// vertical tiles
-		for (int y = 0; y < (TILE_COUNT / 2); y++) {
-			Color color = GameUtils.getRandomColor();
-			int ID = y;
-			Tile t = new Tile(color, 0, ID, 0, (y * tileHeight), tileWidth, tileHeight);
-			tiles.add(t);
-		}
+	/**
+	 * Calls each draw method for redrawing dice, players, and tiles to the
+	 * screen.
+	 * @param g - Graphics to draw to
+	 */
+	public void paintComponent(Graphics g) {
+		g2d = (Graphics2D)g;
+		g2d.clearRect(0, 0, getWidth(), getHeight());
+		g2d.setColor(GameUtils.colorFromHex("#2b2b2b"));
+		g2d.fillRect(0, 0, getWidth(), getHeight());
+		g2d.setColor(Color.CYAN);
+		g2d.drawRect(midRect.x, midRect.y, midRect.width, midRect.height);
+		drawTiles(g2d);
+		dice.draw(g2d);
+		drawPlayers(g2d);
+		g2d.dispose();
+	}
+
+	/**
+	 * Redraws everything to the screen, as defined in paintComponent().
+	 */
+	public void render() {
 		repaint();
+	}
+	
+	/**
+	 * Mouse click event. Checks to see if mouse click pt location is contained
+	 * inside of dice and players. If it is, their state is then updated to 
+	 * reflect this event.
+	 */
+	public void mouseClicked(MouseEvent e) {
+		if (dice.contains(e.getPoint())) {
+			dice.roll(Dice.SIZE);
+		}
+		
+		for (Player p : director.getPlayers()) {
+			if (p.contains(e.getPoint())) {
+				if (p.isSelected()) {	//toggle between isSelected
+					p.setSelected(false);
+				} else {
+					p.setSelected(true);
+				}
+			} else if (p.isSelected()) {
+				if (p.isMoving()) {
+					p.setNewLocation(e.getPoint());	// change to new destination
+				} else {
+					p.moveTo(e.getPoint());	
+				}
+			}
+		}
+	}
+	
+	// unused mouse listener methods
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	
+	/**
+	 * Updates the appearance of all objects in the game such as their positioning and
+	 * widths, if the application has been resized. By designating this into its own
+	 * component listener, we can be more efficient when updating, rather than constantly
+	 * checking in the main update method.
+	 */
+	public void componentResized(ComponentEvent e) {
+		System.out.println("Board resized! Updating stuff!");
+		midRect.x = (getWidth() - midRect.width) / 2;
+		midRect.y = (getHeight() - midRect.height) / 2;
+		resizeTiles();	// update tiles with new size info
+	}
+
+	// unused component listener methods
+	public void componentMoved(ComponentEvent e) {}
+	public void componentShown(ComponentEvent e) {}
+	public void componentHidden(ComponentEvent e) {}
+
+	/**
+	 * Mouse drag event. Checks to see if mouse drag pt location intersects within the
+	 * midrect bounds. If true, the dice can be moved around, so long as the location
+	 * doesn't completely exit the midrect bounds.
+	 */
+	public void mouseDragged(MouseEvent e) {
+		if (midRect.contains(e.getPoint())) {
+			if (midRect.intersects(dice)) {
+				dice.x = e.getPoint().x;
+				dice.y = e.getPoint().y;
+			}
+		}
+	}
+
+	// unused mouse motion listener methods
+	public void mouseMoved(MouseEvent e) {}
+	
+	/**
+	 * Initializes everything and sets up the layout of the board. 
+	 */
+	private void init() {
+		statusPanel = new StatusPanel(director);
+		tiles = new ArrayList<>();
+		midRect = createMidRect();
+		dice = new Dice(600, 400);
+		
+		// setup layout
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTH;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.ipady = 10;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		
+		add(statusPanel, c);
+		loadPlayers();	// setup players for first
+		createTiles();
+		
+		// add event listeners
+		addComponentListener(this);
+		addMouseListener(this);
+		addMouseMotionListener(this);
+	}
+	
+	/**
+	 * Load all new players into the board state from the director.
+	 */
+	private void loadPlayers() {
+		createMidRect();
+		
+		for (int i = 0; i < director.getPlayers().size(); i++) {
+			Player p = director.getPlayers().get(i);
+			
+			p.x = midRect.x * i;
+			p.y = 500;
+		}
+	}
+	
+	/**
+	 * Creates all the game tiles of the game. Currently it is 10 x 5 x 10 x 5.
+	 */
+	private void createTiles() {
+		tiles = new ArrayList<>();
+		// default size 1280 x 720 for initial tile sizing
+		int tileWidth = 1280 / HORIZONTAL_TILE_COUNT;
+		int tileHeight = 720 / VERTICAL_TILE_COUNT;
+		
+		for (int y = 0; y < 5; y++) {
+			for (int x = 0; x < 10; x++) {
+				Color color = GameUtils.getRandomColor();
+				
+				if (y == 0 || y == 4) {	// top and bottom tiles
+					Tile t = new Tile(color, 0, x + y, x, y, tileWidth, tileHeight);
+					tiles.add(t);
+				} else
+				if (x == 0 || x == 9) { // side tiles
+					Tile t = new Tile(color, 0, x + y, x, y, tileWidth, tileHeight);
+					tiles.add(t);
+				}
+			}
+		}
 	}
 	
 	/**
@@ -66,50 +233,42 @@ public class BoardState extends State {
 		return new Rectangle(x, y, width, height);
 	}
 	
+	/**
+	 * Redraws each player to the screen.
+	 * @param g - Graphics to draw to
+	 */
 	private void drawPlayers(Graphics g) {
-		int gap = 100;	// 80 px gap between each player
-		ArrayList<Player> players = director.getPlayers();
-		
-		//TODO fix player offsets
-		for (int i = 0; i < players.size(); i++) {
-			Player p = players.get(i);
-			p.setXPos(50 + midRect.x + gap * i);
-			p.setYPos(50 + midRect.y + 200);
+		for (int i = 0; i < director.getPlayers().size(); i++) {
+			Player p = director.getPlayers().get(i);
 			p.draw(g);
 		}
 	}
 	
-	private void drawTurnIndicator(Graphics g) {
-		
-	}
-	
-	public void paintComponent(Graphics g) {
-		render();
-	}
-
-	public void update() {
-		System.out.println("Turn " + director.getTurn());
-	}
-
-	public void render() {
-		g.clearRect(0, 0, getWidth(), getHeight());
-		for (int i = 0; i < tiles.size(); i++) {	// draws all tiles
+	/**
+	 * Redraws each tile to the screen.
+	 * @param g - Graphics to draw to
+	 */
+	private void drawTiles(Graphics g) {
+		for (int i = 0; i < tiles.size(); i++) {
 			Tile t = tiles.get(i);
 			t.draw(g);
 		}
-		
-		// draw middle box container for players, dice, and turn indicator
-		g.setColor(Color.RED);
-		g.drawRect(midRect.x, midRect.y, midRect.width, midRect.height);
-		
-		// draw players evenly spaced
-		drawPlayers(g);
-		
-		// draw turn count from director
-		drawTurnIndicator(g);
-		
-		//TODO draw dice stuff here
-		dice.roll(4);
-		dice.draw(g);
 	}
+	
+	/**
+	 * Updates the sizing of tiles. This should only be called from componentResized().
+	 * This just ensures that all the tiles will fill the board, depending on the 
+	 * current size of the window.
+	 */
+	private void resizeTiles() {
+		int tileWidth = (getWidth() / HORIZONTAL_TILE_COUNT);
+		int tileHeight = (getHeight() / VERTICAL_TILE_COUNT);
+		
+		for (int i = 0; i < tiles.size(); i++) {
+			Tile t = tiles.get(i);
+			t.width = tileWidth;
+			t.height = tileHeight;
+		}
+	}
+	
 }
